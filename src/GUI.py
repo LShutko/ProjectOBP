278b67b59f8417b91ef655f5b37217c90ae6c9a8
Fijen <klm85303@cs.ad.klmcorp.net>
2020-01-14 14:47:42 +0100

    refactor GUI

--- src/GUI.py
+++ src/GUI.py
@@ -2,18 +2,104 @@ import PySimpleGUI as sg
 import sys
 import matplotlib.pyplot as plt
 
-from os import listdir
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-#from graphs import PlotChart
+
+
+# from graphs import PlotChart
 
 
 class Interface(object):
 
     def __init__(self, settings):
+        self.window = sg.Window('Bacterial spot prediction', self.define_layout(), default_element_size=(30, 1),
+                                grab_anywhere=False)
+        self.graph_refresh = {
+            'Tomato': self.refresh_plots,
+            'Potato': self.refresh_plots,
+            'Pepperbell': self.refresh_plots,
+            'all': self.refresh_plots,
+            'pie_chart': self.refresh_plots,
+            'bar_chart': self.refresh_plots
+        }
+        self.dispatch_dictionary = {
+            'Load the data': self.load,
+            'Source code': self.source_code,
+            'User manual': self.user_manual,
+            'Export': self.export
+        }
         self.settings = settings
-        self.directory_root = './input/'  # location of the training set data
+        self.input_directory = './input/'  # location of the training set data
 
-    def define_layout(self):
+    def create_window(self, layout):
+        return
+
+    # Matplotlib helper code for embedded canvas
+    def draw_figure(self, canvas, figure):
+        figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+        figure_canvas_agg.draw()
+        figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+        return figure_canvas_agg
+
+    def load(self):
+        fname = sys.argv[1] if len(sys.argv) > 1 else sg.popup_get_file('Document to open')
+        if not fname:
+            sg.popup("Cancel", "No filename supplied")
+            raise SystemExit("Cancelling: no filename supplied")
+        else:
+            print(fname)
+
+    def load_training(self):
+        names, sizes = self.get_data(self.input_directory)
+        fig = plt.gcf()  # if using Pyplot then get the figure from the plot
+
+        figure_x, figure_y, figure_w, figure_h = fig.bbox.bounds
+        my_canvas = self.window.FindElement('canvas')
+        #    my_canvas.set_size(figure_w, figure_h)
+        fig_photo = self.draw_figure(my_canvas.TKCanvas, fig)
+        return fig_photo
+
+    def source_code(self):
+        sg.SetOptions(text_justification='right')
+        layout = [[sg.Text('Link to the GitHub', font=('Helvetica', 10))]]
+
+        window = sg.Window('Source code', layout, font=("Helvetica", 12))
+
+        event, values = window.read()
+
+    def user_manual(self):
+        sg.SetOptions(text_justification='right')
+        layout = [[sg.Text('Here will be some description of user manual (help)', font=('Helvetica', 10))]]
+
+        window = sg.Window('Source code', layout, font=("Helvetica", 12))
+
+        event, values = window.read()
+
+    def export(self):
+        sg.SetOptions(text_justification='right')
+        layout = [[sg.Text('Here files will be exported', font=('Helvetica', 10))]]
+
+        window = sg.Window('Source code', layout, font=("Helvetica", 12))
+
+        event, values = window.read()
+
+    def refresh_plots(self, tkfig):
+        ctr_plant = ['Tomato', 'Potato', 'Pepperbell', 'all']
+
+        if self.window.FindElement('pie_chart').Get():
+            chart_type = 'pie'
+        else:
+            chart_type = 'bar'
+        fig = plt.gcf()  # if using Pyplot then get the figure from the plot
+        fig.clf()
+        names, sizes = self.get_data(self.input_directory)
+        for plant in ctr_plant:
+            if self.window.FindElement(plant).Get() > 0:
+                print('TODO')
+                # TODO: descriptive_text, plot = PlotChart(names, sizes, plant, chart_type)
+        tkfig.draw()
+
+    @staticmethod
+    def define_layout():
         sg.ChangeLookAndFeel('Material1')
 
         home_tab_layout = [
@@ -63,93 +149,3 @@ class Interface(object):
         ]
 
         return layout
-
-    def create_window(self, layout):
-        self.dispatch_dictionary = {
-            'Load the data': self.load,
-            'Source code': self.Source_code,
-            'User manual': self.User_manual,
-            'Export': self.Export
-        }
-        self.graph_refresh = {
-            'Tomato': self.refresh_plots,
-            'Potato': self.refresh_plots,
-            'Pepperbell': self.refresh_plots,
-            'all': self.refresh_plots,
-            'pie_chart': self.refresh_plots,
-            'bar_chart': self.refresh_plots
-        }
-        self.window = sg.Window('Bacterial spot prediction', layout, default_element_size=(30, 1), grab_anywhere=False)
-        return
-
-    # Matplotlib helper code for embedded canvas
-    def draw_figure(self, canvas, figure, loc=(0, 0)):
-        figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
-        figure_canvas_agg.draw()
-        figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
-        return figure_canvas_agg
-
-
-    def load(self):
-        fname = sys.argv[1] if len(sys.argv) > 1 else sg.popup_get_file('Document to open')
-        if not fname:
-            sg.popup("Cancel", "No filename supplied")
-            raise SystemExit("Cancelling: no filename supplied")
-        else:
-            print(fname)
-
-
-    def load_training(self):
-        names, sizes = self.get_data(self.directory_root)
-        fig = plt.gcf()  # if using Pyplot then get the figure from the plot
-        #PlotChart(names, sizes, "all", "pie")
-
-        figure_x, figure_y, figure_w, figure_h = fig.bbox.bounds
-        my_canvas = self.window.FindElement('canvas')
-        #    my_canvas.set_size(figure_w, figure_h)
-        fig_photo = self.draw_figure(my_canvas.TKCanvas, fig)
-        return fig_photo
-
-
-    def source_code(self):
-        sg.SetOptions(text_justification='right')
-        layout = [[sg.Text('Link to the GitHub', font=('Helvetica', 10))]]
-
-        window = sg.Window('Source code', layout, font=("Helvetica", 12))
-
-        event, values = window.read()
-
-
-    def user_manual(self):
-        sg.SetOptions(text_justification='right')
-        layout = [[sg.Text('Here will be some description of user manual (help)', font=('Helvetica', 10))]]
-
-        window = sg.Window('Source code', layout, font=("Helvetica", 12))
-
-        event, values = window.read()
-
-
-    def export(self):
-        sg.SetOptions(text_justification='right')
-        layout = [[sg.Text('Here files will be exported', font=('Helvetica', 10))]]
-
-        window = sg.Window('Source code', layout, font=("Helvetica", 12))
-
-        event, values = window.read()
-
-
-    def refresh_plots(self, tkfig):
-        ctr_plant = ['Tomato', 'Potato', 'Pepperbell', 'all']
-
-        if self.window.FindElement('pie_chart').Get():
-            chart_type = 'pie'
-        else:
-            chart_type = 'bar'
-        fig = plt.gcf()  # if using Pyplot then get the figure from the plot
-        fig.clf()
-        names, sizes = self.get_data(self.directory_root)
-        for plant in ctr_plant:
-            if self.window.FindElement(plant).Get() > 0:
-                print('TODO')
-                #descriptive_text, plot = PlotChart(names, sizes, plant, chart_type)
-        tkfig.draw()
